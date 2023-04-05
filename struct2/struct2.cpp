f6fe3ed2365320321e6246183191315a417f358d
IAMHUDA <hudamiftahul362@gmail.com>
2023-04-05 15:35:20 +0700

    Add project files.

---
+++ struct2/struct2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa
+{
+	string NIM;
+	string Nama;
+	DetailAlamat Alamat;
+	int umur;
+};
+
+int main()
+{
+	Mahasiswa mhs;
+
+	cout << "Masukan Nim = ";
+	cin >> mhs.NIM;
+	cout << "Masukan Nama = ";
+	cin >> mhs.Nama;
+	cout << "Alamat = " << endl;
+	cout << "\n\tmasukkan desa = ";
+	cin >> mhs.Alamat.desa;
+	cout << "\tmasukkan kota = ";
+	cin >> mhs.Alamat.kota;
+	cout << "Masukan umur = ";
+	cin >> mhs.umur;
+
+	cout << "\nNIm = " << mhs.NIM;
+	cout << "\nNama = " << mhs.Nama;
+	cout << "\nalamat desa = " << mhs.Alamat.desa;
+	cout << "\nalamat kota = " << mhs.Alamat.kota;
+	cout << "\numur = " << mhs.umur;
+
+
+}
---
+++ struct3/struct3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	char desa[20];
+	char kota[20];
+};
+
+struct Mahasiswa
+{
+	char NIM[12];
+	char Nama[20];
+	DetailAlamat Alamat;
+	int umur;
+};
+
+int main()
+{
+	Mahasiswa mhs[2];
+
+	for (int i = 0; i < 2; i++) {
+
+		cout << "Masukan Nim = ";
+		cin.getline(mhs[i].NIM, 12);
+		cout << "Masukan Nama = ";
+		cin.getline(mhs[i].Nama, 20);
+		cout << "Alamat = " << endl;
+		cout << "\n\tmasukkan desa = ";
+		cin.getline(mhs[i].Alamat.desa, 20);
+		cout << "\tmasukkan kota = ";
+		cin.getline(mhs[i].Alamat.kota, 20);
+		cout << "Masukan umur = ";
+		cin >> mhs[i].umur;
+		cin.ignore(1,'\n');
+	}
+	for (int i = 0; i < 2; i++) {
+		cout << "\nNIm = " << mhs[i].NIM;
+		cout << "\nNama = " << mhs[i].Nama;
+		cout << "\nalamat desa = " << mhs[i].Alamat.desa;
+		cout << "\nalamat kota = " << mhs[i].Alamat.kota;
+		cout << "\numur = " << mhs[i].umur;
+		cout << endl;
+	}
+
+}
